a3a990a670adf771af0177df409ff38fb1df4a33
sam-micha <r.samuelmicha@gmail.com>
2022-02-21 21:34:11 +0530

    Hi Madristas

---
+++ src/whatsapp/School1.java
@@ -0,0 +1,21 @@
+package whatsapp;
+
+public class School1 {
+	
+	public static void main(String[] args) {
+		
+		int age=16;
+		
+		if(age>=18)
+		{
+			System.out.println("vote");
+		}
+		
+		else
+		{
+			System.out.println("no vote");
+		}
+	
+	}
+
+}
